db564fe59a7f65948de26d6585c4ac4de35babfc
andrey <egorovandreyrm@gmail.com>
2025-04-29 12:41:59 +0300

    third party streams improved

--- src/components/views/messages/MStreamBody.tsx
+++ src/components/views/messages/MStreamBody.tsx
@@ -94,37 +94,8 @@ export default class MStreamBody extends React.Component<IBodyProps, IState> {
         }
     }
 
-    private fixUrl(url: string): string {
-        if (url.startsWith("srt://t3.rpipro.xyz:10080?streamid=#!::r=t3/livestream_")) {
-            // http://t3.rpipro.xyz:1985/rtc/v1/whep/?app=t3&stream=livestream_1745320301907
-
-            // let fixedUrl = url.replace(
-            //     "srt://t3.rpipro.xyz:10080?streamid=#!::r=t3/livestream_",
-            //     "http://t3.rpipro.xyz:1985/rtc/v1/whep/?app=t3&stream=livestream_"
-            // )
-            //
-            // // http://t3.rpipro.xyz:4567/player.html?stream_id=1745320301907
-            //
-            // fixedUrl = fixedUrl.replace(
-            //     ",m=publish",
-            //     ""
-            // )
-
-            const underscoreIndex = url.lastIndexOf("_")
-            if (underscoreIndex != -1) {
-                let fixedUrl =  "http://t3.rpipro.xyz:4567/player.html?stream_id=" + url.substring(underscoreIndex + 1)
-                fixedUrl = fixedUrl.replace(
-                    ",m=publish",
-                    ""
-                )
-
-                return fixedUrl
-            }
-
-            return url
-        }
-
-        return url
+    private getStreamViewerUrl(streamApp: string, streamId: string): string {
+        return `http://t3.rpipro.xyz:4567/player.html?app=${streamApp}&stream=${streamId}`
     }
 
     public render(): ReactNode {
@@ -139,7 +110,33 @@ export default class MStreamBody extends React.Component<IBodyProps, IState> {
         //     <span className="mx_MStreamBody_edited"> ({_t("common|edited")})</span>
         // ) : null;
 
-        const streamUrl = this.fixUrl(streamEvent.stream_url.text)
+        const streamUrl = this.getStreamViewerUrl(
+            streamEvent.stream_app.text,
+            streamEvent.stream_id.text
+        )
+
+        const openInNewTab = (url: string): void => {
+            const newWindow = window.open(url, '_blank', 'width=496,height=892')
+            if (newWindow) newWindow.opener = null
+        }
+
+        const onClickUrl = (): (() => void) => () => openInNewTab(streamUrl)
+
+        const thirdPartyStreamInfo = (
+            <div>
+                {_t("stream|third_party_stream_info")}
+                <br /><br />
+                {_t("stream|third_party_srt_stream_info", {
+                    stream_app: streamEvent.stream_app.text,
+                    stream_id: streamEvent.stream_id.text
+                })}
+                <br /><br />
+                {_t("stream|third_party_rtmp_stream_info", {
+                    stream_app: streamEvent.stream_app.text,
+                    stream_id: streamEvent.stream_id.text
+                })}
+            </div>
+        )
 
         return (
             <div className="mx_MStreamBody">
@@ -150,9 +147,13 @@ export default class MStreamBody extends React.Component<IBodyProps, IState> {
                         )
                         + streamEvent.description.text}
 
-                    <div><a target="_blank" rel="noopener noreferrer" href={streamUrl}>{(streamEvent.third_party? streamUrl : _t("stream|view_stream"))}</a></div>
+                    <br />
+                    {streamEvent.third_party? thirdPartyStreamInfo : undefined}
                 </h4>
 
+                <div>
+                    <button onClick={onClickUrl()}>{_t("stream|view_stream")}</button>
+                </div>
             </div>
         );
     }
